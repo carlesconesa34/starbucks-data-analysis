faaa0a07526838e88b8d6ac021d9b552c106f65d
Carles Conesa Mañosa <businessccm34@gmail.com>
2026-03-04 20:02:49 +0100

    Fix SQL cleaning script errors.

--- sql/starbucks_cleaning.sql
+++ sql/starbucks_cleaning.sql
@@ -2,11 +2,10 @@ CREATE OR REPLACE TABLE orders AS
 SELECT 
     customer_id AS Customer_ID,
     order_id AS Order_ID,
-    order_date AS Order_Date,
-    order_time AS Order_Time, 
+    order_time AS Order_Time,
+    order_time AS Order_Time,
     day_of_week AS Day_of_Week,
     order_channel AS Order_Channel,
-
     store_id AS Store_ID,
     store_location_type AS Store_Location_Type,
     region AS Region,
@@ -20,7 +19,7 @@ SELECT
     has_food_item AS Has_Food,
     order_ahead AS Ordered_Ahead,
     fulfillment_time_min AS Prep_Time_Min,
-    customer_satisfaction AS Customer_Satisfaction,
+    customer_satisfaction AS Customer_Satisfaction
 FROM 'data/processed/starbucks_customer_ordering_patterns_cleaned.csv';
 
 COPY (SELECT * FROM orders) TO 'data/processed/starbucks_cleaned_sql.csv' (HEADER, DELIMITER ',');
