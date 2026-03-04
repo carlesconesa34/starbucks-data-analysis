caf5e023634ef8615396cfa9bb9e3ec261b4cd52
Carles Conesa Mañosa <businessccm34@gmail.com>
2026-03-04 21:02:50 +0100

    Fix errors, comments and update gitignore.

--- sql/starbucks_cleaning.sql
+++ sql/starbucks_cleaning.sql
@@ -9,16 +9,16 @@ SELECT
     store_id AS Store_ID,
     store_location_type AS Store_Location_Type,
     region AS Region,
-    customer_age_group AS Age_Group,
-    customer_gender AS Gender,
-    is_rewards_member AS Rewards_Member,
+    customer_age_group AS Customer_Age_Group,
+    customer_gender AS Customer_Gender,
+    is_rewards_member AS Is_Rewards_Member,
     cart_size AS Cart_Size,
     num_customizations AS Num_Customizations,
     total_spend AS Total_Spend,
-    fulfillment_time_min AS Prep_Time_Min,
+    fulfillment_time_min AS Fullfillment_Time_Min,
     drink_category AS Drink_Category,
-    has_food_item AS Has_Food,
-    order_ahead AS Ordered_Ahead,
+    has_food_item AS Has_Food_Item,
+    order_ahead AS Order_Ahead,
     customer_satisfaction AS Customer_Satisfaction
 FROM 'data/processed/starbucks_customer_ordering_patterns_cleaned.csv';
 COPY (SELECT * FROM orders) TO 'data/processed/starbucks_cleaned_sql.csv' (HEADER, DELIMITER ',');
